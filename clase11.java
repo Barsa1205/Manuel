8a7e81e3bd07b035e920acd9d33b50832afb8635
Manuel Velásquez <manuelvelasquez1203@gmail.com>
2025-04-21 22:04:56 +0200

    Add files via upload

---
+++ clase11.java
@@ -0,0 +1,15 @@
+package es.goya;
+
+public class clase11 {
+    public String metodo1() {
+        return "metodo1";
+    }
+
+    public String metodo2() {
+        return "metodo2";
+    }
+
+    public String metodoA() {
+        return "metodoA";
+    }
+}
